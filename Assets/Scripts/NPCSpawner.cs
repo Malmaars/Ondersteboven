a83203ca782b5bebfc4d557431a43ca70f5fb43d
Smos Bois <simondeb1505@gmail.com>
2022-01-30 04:41:24 +0100

    NPCs added

--- Assets/Scripts/NPCSpawner.cs
+++ Assets/Scripts/NPCSpawner.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class NPCSpawner : MonoBehaviour
 {
     //Mind position and rotation
-    GameObject npcPrefab;
+    public GameObject npcPrefab;
 
     public Transform[] PossibleNPCLocations;
     bool[] isSomeoneSittingThere;
@@ -23,12 +23,18 @@ public class NPCSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //51 zitplaatsen. 1 mogelijke zitplek per stoel. en 1 zitplek per cabine
-        //26 zijn gevuld
+        //32 zitplaatsen. 1 mogelijke zitplek per stoel. en 1 zitplek per cabine
+        //16 zijn gevuld
 
+        isSomeoneSittingThere = new bool[PossibleNPCLocations.Length];
+
+        foreach (Transform loc in PossibleNPCLocations)
+        {
+            loc.gameObject.SetActive(false);
+        }
         //give random destination + random text
         //random accessoires
-        for (int i = 0; i < 26; i++)
+        for (int i = 0; i < 11; i++)
         {
             SpawnNPC();
         }
@@ -36,8 +42,6 @@ public class NPCSpawner : MonoBehaviour
 
     void SpawnNPC()
     {
-        isSomeoneSittingThere = new bool[PossibleNPCLocations.Length];
-
         GameObject npc = Instantiate(npcPrefab);
 
         int randomInt = UnityEngine.Random.Range(0, PossibleNPCLocations.Length);
@@ -49,13 +53,18 @@ public class NPCSpawner : MonoBehaviour
 
         //give random location
         npc.transform.position = PossibleNPCLocations[randomInt].position;
+        npc.transform.rotation = PossibleNPCLocations[randomInt].rotation;
         isSomeoneSittingThere[randomInt] = true;
 
-        randomInt = UnityEngine.Random.Range(0, DifferentLines.Length);
+        //randomInt = UnityEngine.Random.Range(0, DifferentLines.Length);
 
-        //random text & fitting destination
-        npc.GetComponent<NPC>().myLine = DifferentLines[randomInt].line;
-        npc.GetComponent<NPC>().destination = DifferentLines[randomInt].destination;
+        ////random text & fitting destination
+        //npc.GetComponent<NPC>().myLine = DifferentLines[randomInt].line;
+        //npc.GetComponent<NPC>().destination = DifferentLines[randomInt].destination;
+
+        //set a random accessory
+        randomInt = UnityEngine.Random.Range(3, 16);
+        npc.transform.GetChild(randomInt).gameObject.SetActive(true);
 
     }
 }
